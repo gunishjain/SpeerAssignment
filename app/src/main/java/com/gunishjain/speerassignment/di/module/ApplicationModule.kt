f6894146f9cdfa1adc947601efd869b5f630105e
gunishjain <gunish.jain@gmail.com>
2024-01-03 22:30:27 +0530

    Add Cache Interceptor

--- app/src/main/java/com/gunishjain/speerassignment/di/module/ApplicationModule.kt
+++ app/src/main/java/com/gunishjain/speerassignment/di/module/ApplicationModule.kt
@@ -3,12 +3,16 @@ package com.gunishjain.speerassignment.di.module
 import com.gunishjain.speerassignment.data.api.NetworkService
 import com.gunishjain.speerassignment.di.BaseUrl
 import com.gunishjain.speerassignment.utils.AppConstant
+import com.gunishjain.speerassignment.utils.CacheInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import javax.inject.Singleton
 
 
@@ -26,12 +30,29 @@ class ApplicationModule {
 
     @Provides
     @Singleton
+    fun provideCacheInterceptor(): CacheInterceptor =
+        CacheInterceptor()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(cacheInterceptor: CacheInterceptor): OkHttpClient {
+        val cacheLoc = File("/var/tmp/okhttp")
+        return OkHttpClient().newBuilder()
+            .cache(Cache(File(cacheLoc, "http-cache"), 10L * 1024L * 1024L))
+            .addNetworkInterceptor(cacheInterceptor).build()
+    }
+
+
+    @Provides
+    @Singleton
     fun provideNetworkService(
         @BaseUrl baseUrl: String,
+        okHttpClient: OkHttpClient,
         gsonConverterFactory: GsonConverterFactory
     ): NetworkService {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
+            .client(okHttpClient)
             .addConverterFactory(gsonConverterFactory)
             .build()
             .create(NetworkService::class.java)
